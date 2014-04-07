b2e34c925fa0bd526cf60cf5b66fb4846a2da16b
angela ebirim <angelaebirim@Jacks-MacBook-Air-2.local>
2014-04-07 13:01:21 +0100

    Phase III ducktyping exercise

--- lib/toy_factory/dealer.rb
+++ lib/toy_factory/dealer.rb
@@ -1,4 +1,27 @@
 module ToyFactory
   class Dealer
+      attr_reader :order
+
+      def initialize(order)
+          @order = order
+      end
+
+
+      def make_order
+       if order == 'toy'
+           order
+       elsif order == 'car'
+           order
+       elsif order == 'robot'
+           order
+
+       elsif order == 'puzzle'
+            order
+       else
+            order == 'waterproof_toy'
+            order
+       end  
+
+      end    
   end
 end
